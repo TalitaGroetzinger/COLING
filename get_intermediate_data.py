2b632b6f6b6e67949576c527a93904e6f9420ea1
Talita Anthonio <talitaanthonio@guest-8d46a88c.on.site.uni-stuttgart.net>
2020-03-03 17:07:14 +0100

    submit updates

---
+++ get_intermediate_data.py
@@ -0,0 +1,13 @@
+import pickle
+import nltk
+
+# use this script to do everything but for intermediate insertions
+# work with the pandas dataframe
+
+
+def main():
+    corrections = pickle.load(open("./data/real_corrections.pickle", "rb"))
+    # flatten the dictionary
+
+
+main()
