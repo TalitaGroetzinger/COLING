539a7b92bdf31ef162406416c206e8c2c23517d7
Talita Anthonio <talitaanthonio@guest-8d46a88c.on.site.uni-stuttgart.net>
2020-03-06 10:49:07 +0100

    create all_corrections_wikihow_v6.json with just the nouns

--- get_intermediate_data.py
+++ get_intermediate_data.py
@@ -5,6 +5,7 @@ import pandas as pd
 from tqdm import tqdm
 from wikihowtools.add_linguistic_info import compute_char_distance
 import json
+from progress.bar import Bar
 
 
 def tag_data(x):
@@ -20,8 +21,10 @@ def filter_df(df, edit_distance_value=2):
       - a word (or more words) that has changed into another word and this change has a minimum character-based edit-distance of 3
 
     """
+    bar = Bar('Processing', max=len(df))
     collection = []
     for index, row in df.iterrows():
+        bar.next()
         differences = []
         tags = []
         tags_source = [word_pos_pair[1]
@@ -34,21 +37,22 @@ def filter_df(df, edit_distance_value=2):
                     distance = compute_char_distance(
                         source[0], target[0])
                     if distance > edit_distance_value:
-                        differences.append([source, target])
-                        assert source[1] == target[1]
-                        tags.append(source[1])
+                        if 'NN' == source[1] or 'NNS' == source[1] or 'NNP' == source[1]:
+                            differences.append([source, target])
+                            assert source[1] == target[1]
+                            tags.append(source[1])
         if differences != []:
-            if 'NN' in tags or 'NNS' in tags or 'NNP' in tags:
-                d = {"Filename": row['Article_Name'], "Source_Line_Tagged": row['Source_Line_Tagged'],
-                     "Target_Line_Tagged": row['Target_Line_Tagged'], "Differences": differences,  "Tags": tags, "Revision_Length": row['Revision_Length']}
-                collection.append(d)
+            d = {"Filename": row['Article_Name'], "Source_Line_Tagged": row['Source_Line_Tagged'],
+                 "Target_Line_Tagged": row['Target_Line_Tagged'], "Differences": differences,  "Tags": tags, "Revision_Length": row['Revision_Length']}
+            collection.append(d)
+    bar.finish()
     return collection
 
 
 def main():
     df = pd.read_pickle(
         './data/wikihow_instructional_text_ordered_no_cycle_v6_tagged_source_FINAL.pickle')
-    #df = pd.read_pickle('subset.pickle')
+    # df = pd.read_pickle('subset.pickle')
     print("Make corrections ...")
     corrections = filter_df(df)
     print(len(corrections))
