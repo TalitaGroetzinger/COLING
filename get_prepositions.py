dc30fd6ec39c5b0e21c60254fe2e0958b1ab9f25
Talita Anthonio <talitaanthonio@MacBook-Pro-van-Talita.local>
2020-03-09 21:27:19 +0100

    identify prepositional phrases

--- get_prepositions.py
+++ get_prepositions.py
@@ -1,43 +1,62 @@
 from wikihowtools.add_linguistic_info import read_json
 import json
 from progress.bar import Bar
+import spacy
+
+spacy_tagger = spacy.load('en_core_web_sm')
 
 
-def diff_tags(list_of_wikihow_instances):
+def get_pps_from_sent(sent, token_to_look_for='ADP'):
     """
-        First argument: target_line
-        Second argument: source_line
+      Input: A string
+      Output: A list of preposition phrases occuring in that string.
     """
-    possible_pp_insertions = []
-    bar = Bar('Processing', max=len(list_of_wikihow_instances))
-    for wikihow_instance in list_of_wikihow_instances:
-        target = wikihow_instance['Target_Tagged']
-        source = wikihow_instance['Source_tagged']
-        target_words = [pair[0] for pair in target]
-        source_words = [pair[0] for pair in source]
-        source_line_set = set(source_words)
-        insertions = [[word, tag]
-                      for word, tag in target if word not in source_line_set]
-        indexes = [target.index([word, tag])
-                   for word, tag in target if word not in source_line_set]
+    parsed_sent = spacy_tagger(sent)
+    pps = []
+    for token in parsed_sent:
+        # Try this with other parts of speech for different subtrees.
+        if token.pos_ == token_to_look_for:
+            pp = ' '.join([tok.orth_ for tok in token.subtree])
+            pps.append(pp)
+    return pps
 
-        insertion_tags = [word_tag[1] for word_tag in insertions]
+
+def check_difs(list_of_wikihow_instance):
+    collection = []
+    bar = Bar('Processing', max=len(list_of_wikihow_instance))
+    for wikihow_instance in list_of_wikihow_instance:
+        diff_preps = []
+        source_tokens = " ".join(wikihow_instance['Source_Tokenized'])
+        target_tokens = " ".join(wikihow_instance['Target_Tokenized'])
+        PP_in_source = get_pps_from_sent(source_tokens)
+        PP_in_target = get_pps_from_sent(target_tokens)
+        source_pps_set = set(PP_in_source)
+        non_prepositions = ["if", "that"]
+        for token in PP_in_target:
+            if token not in source_pps_set:
+                if token.lower() not in non_prepositions:
+                    diff_preps.append(token)
+                    wikihow_instance['Possible_PPS_Insertions'] = diff_preps
+        if diff_preps != []:
+            del wikihow_instance['All_Versions']
+            del wikihow_instance['Base_Sentence']
+            del wikihow_instance['Revisions']
+            del wikihow_instance['Source_tagged']
+            del wikihow_instance['Target_Tagged']
+            del wikihow_instance['Key']
+            collection.append(wikihow_instance)
         bar.next()
-        if 'IN' in insertion_tags:
-            wikihow_instance['Insertions'] = insertions
-            wikihow_instance['Indexes'] = indexes
-            possible_pp_insertions.append(wikihow_instance)
     bar.finish()
-    return possible_pp_insertions
+    return collection
 
 
 def main():
     insertions = read_json('./data/insertions.json')
-    print("length of insertions", len(insertions))
-    new_insertions = diff_tags(insertions)
-    print("Possible pp-insertions", len(new_insertions))
-    with open('potential_preposition_insertions.json', 'w') as json_out:
+    new_insertions = check_difs(insertions)
+    with open('pp_insertions_new.json', 'w') as json_out:
         json.dump(new_insertions, json_out)
+    print("length of insertions", len(insertions))
+    print("Possible pp-insertions", len(new_insertions))
 
 
 main()
