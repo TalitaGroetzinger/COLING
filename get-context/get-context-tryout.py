6b35e8b37ff0febff1552e903e4af550e0c86dea
Talita Anthonio <talitaanthonio@MacBook-Pro-van-Talita.local>
2020-03-17 11:56:25 +0100

    update gitignore

--- get-context/get-context-tryout.py
+++ get-context/get-context-tryout.py
@@ -4,17 +4,15 @@ import pickle
 
 
 collection = {}
-for filename in os.listdir('dev-files-example'):
+for filename in os.listdir('dev-files'):
     path = "dev-files-example/{0}".format(filename)
+    print(path)
     with bz2.open(path, "rt") as bz_file:
         file_in_dict_format = {}
         for counter, line in enumerate(bz_file, 1):
             if line != '\n':
                 file_in_dict_format[counter] = line.strip('\n').strip()
     collection[filename] = file_in_dict_format
-
-for key, _ in collection.items():
-    print(collection[key])
 
 with open('dev-files-in-dict-format.pickle', 'wb') as pickle_out:
     pickle.dump(collection, pickle_out)
