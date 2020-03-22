7e844aa2fa1010373eb3cc5dc7e1c43b787ba354
Talita Anthonio <talitaanthonio@MacBook-Pro-van-Talita.local>
2020-03-22 17:57:35 +0100

    make files with context that contains exactly 5 sentences for same-noun-modifications

---
+++ get-context/get-window-5.sh
@@ -0,0 +1,2 @@
+python process_context.py --input ../classification-scripts/same-noun-modifications/SAME-NOUN-MODIFICATIONS-TRAIN.JSON --output ../classification-scripts/same-noun-modifications/SAME-NOUN-MODIFICATIONS-TRAIN-5.JSON 
+python process_context.py --input ../classification-scripts/same-noun-modifications/SAME-NOUN-MODIFICATIONS-TEST.JSON --output ../classification-scripts/same-noun-modifications/SAME-NOUN-MODIFICATIONS-TEST-5.JSON 
