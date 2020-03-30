59ff4591404b50e885f5e37c723e26dc870daed1
Talita Anthonio <talitaanthonio@MacBook-Pro-van-Talita.local>
2020-03-30 11:06:10 +0200

    add script clean_data.py to remove key-value pairs from Wikihow_tokenized_v5.json

---
+++ all-wikihow/clean_data.py
@@ -0,0 +1,45 @@
+import json
+from progress.bar import Bar
+
+
+def clean_dict(list_of_wikihow_instances):
+    filtered_data = []
+    bar = Bar("Processing ...", max=len(list_of_wikihow_instances))
+    for elem in list_of_wikihow_instances:
+        bar.next()
+        # delete elements which are unnecessary
+        del elem['Base_Sentence']
+        del elem['Revisions']
+        del elem['Source_Tokenized']
+        del elem['Target_Tokenized']
+        del elem['All_Versions']
+
+        # rename source_tagged
+        elem['Source_Tagged'] = elem['Source_tagged']
+        del elem['Source_tagged']
+        elem.append(filtered_data)
+    bar.finish()
+    try:
+        assert len(filtered_data) == len(list_of_wikihow_instances)
+    except AssertionError:
+        print("length is unequal: filtered: {0} \t unfiltered: {1} ".format(
+            len(filtered_data), len(list_of_wikihow_instances)))
+    return filtered_data
+
+
+def main():
+    path_to_file = '../wikihowtools/data/Wikihow_tokenized_v5.json'
+    print("load data .... ")
+    with open(path_to_file, 'r') as json_in:
+        content = json.load(json_in)
+
+    # filter the data
+    print("clean data ")
+    filtered_data = clean_dict(content)
+
+    # write to new file
+    with open("../wikihowtools/data/Wikihow_tokenized_v5_cleaned.json", 'w') as json_out:
+        json.dump(filtered_data, json_out)
+
+
+main()
