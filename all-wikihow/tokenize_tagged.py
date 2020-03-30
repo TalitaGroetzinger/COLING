90ecb827fbbfe6d57bb17bdcf264e0ec7d01e091
Talita Anthonio <talitaanthonio@MacBook-Pro-van-Talita.local>
2020-03-30 19:47:04 +0200

    add temporary script to untokenize the tagged source and tagged target

---
+++ all-wikihow/tokenize_tagged.py
@@ -0,0 +1,24 @@
+import json
+from progess.bar import Bar
+
+#path = './Wikihow_tokenized_v5_cleaned_splits.json'
+path = './subset-splits.json'
+with open(path, 'r') as json_in:
+    content = json.load(json_in)
+
+new = []
+bar = Bar('Processing ...', max=len(content))
+for wikihow_instance in content:
+    bar.next()
+    wikihow_instance['Source_Tokens'] = [pair[0]
+                                         for pair in wikihow_instance['Source_Tagged']]
+    wikihow_instance['Target_Tokens'] = [pair[0]
+                                         for pair in wikihow_instance['Target_Tagged']]
+    del wikihow_instance['Source_Tagged']
+    del wikihow_instance['Target_Tagged']
+    new.append(wikihow_instance)
+bar.finish()
+
+filename_to_write = "subset-splits-tokenized"
+with open(filename_to_write, 'w') as json_out:
+    json.dump(new, json_out)
