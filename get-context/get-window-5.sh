410dc8590a390036784ca2662a2d41c421ee20a6
Talita Anthonio <talitaanthonio@MacBook-Pro-van-Talita.local>
2020-03-30 09:43:19 +0200

    update get-window-5.sh to use get_sentences_from_context.py

--- get-context/get-window-5.sh
+++ get-context/get-window-5.sh
@@ -1,3 +1,3 @@
-python filter_context.py --input ../classification-scripts/same-noun-modifications/SAME-NOUN-MODIFICATIONS-TEST-5-new.json --output ../classification-scripts/same-noun-modifications/SAME-NOUN-MODIFICATIONS-TEST-5-v2.json
-python filter_context.py --input ../classification-scripts/same-noun-modifications/SAME-NOUN-MODIFICATIONS-TRAIN-5-new.json --output ../classification-scripts/same-noun-modifications/SAME-NOUN-MODIFICATIONS-TRAIN-5-v2.json
-python filter_context.py --input ../classification-scripts/same-noun-modifications/SAME-NOUN-MODIFICATIONS-DEV-5-new.json --output ../classification-scripts/same-noun-modifications/SAME-NOUN-MODIFICATIONS-DEV-5-v2.json
+python get_sentences_from_context.py --input ../classification-scripts/same-noun-modifications/SAME-NOUN-MODIFICATIONS-TEST-5-new.json --output ../classification-scripts/same-noun-modifications/SAME-NOUN-MODIFICATIONS-TEST-5-v2.json
+python get_sentences_from_context.py --input ../classification-scripts/same-noun-modifications/SAME-NOUN-MODIFICATIONS-TRAIN-5-new.json --output ../classification-scripts/same-noun-modifications/SAME-NOUN-MODIFICATIONS-TRAIN-5-v2.json
+python get_sentences_from_context.py --input ../classification-scripts/same-noun-modifications/SAME-NOUN-MODIFICATIONS-DEV-5-new.json --output ../classification-scripts/same-noun-modifications/SAME-NOUN-MODIFICATIONS-DEV-5-v2.json
