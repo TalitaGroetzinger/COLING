d560497872d24f366af6011fea6b7aafce75dc4c
Talita Anthonio <talitaanthonio@MacBook-Pro-van-Talita.local>
2020-03-31 12:38:47 +0200

    Add scripts to make one dataset for the noun modifications.

---
+++ all-wikihow/make_sets.py
@@ -0,0 +1,50 @@
+import json
+
+
+def get_paths(different_nouns=True):
+    if different_nouns:
+        path_to_train = '../classification-scripts/different-noun-modifications/DIFF-NOUN-MODIFICATIONS-TRAIN-5-new.JSON'
+        path_to_dev = '../classification-scripts/different-noun-modifications/DIFF-NOUN-MODIFICATIONS-DEV-5-new.JSON'
+        path_to_test = '../classification-scripts/different-noun-modifications/DIFF-NOUN-MODIFICATIONS-TEST-5-new.JSON'
+    else:
+        path_to_train = '../classification-scripts/same-noun-modifications/SAME-NOUN-MODIFICATIONS-TRAIN-5-new.JSON'
+        path_to_dev = '../classification-scripts/same-noun-modifications/SAME-NOUN-MODIFICATIONS-DEV-5-new.JSON'
+        path_to_test = '../classification-scripts/same-noun-modifications/SAME-NOUN-MODIFICATIONS-TEST-5-new.JSON'
+    return path_to_train, path_to_dev, path_to_test
+
+
+def write_to_json(file_name, list_to_dump):
+    with open(file_name, 'w') as json_out:
+        json.dump(list_to_dump, json_out)
+
+
+def merge_lists(path_to_list1, path_to_list2):
+    with open(path_to_list1, 'r') as json_in1:
+        list1 = json.load(json_in1)
+    with open(path_to_list2, 'r') as json_in2:
+        list2 = json.load(json_in2)
+    print(len(list1+list2))
+    return list1 + list2
+
+
+def main():
+    train_diff, dev_diff, test_diff = get_paths(True)
+    train_same, dev_same, test_same = get_paths(False)
+
+    all_train = merge_lists(train_diff, train_same)
+    all_dev = merge_lists(dev_diff, dev_same)
+    all_test = merge_lists(test_diff, test_same)
+
+    path_to_dir = '../classification-scripts/noun-modifications'
+    print("merge train ... ")
+    write_to_json(
+        "{0}/noun-modifications-train-5-new.json".format(path_to_dir), all_train)
+    print("Merge dev ..")
+    write_to_json(
+        "{0}/noun-modifications-dev-5-new.json".format(path_to_dir), all_dev)
+    print("Merge test")
+    write_to_json(
+        "{0}/noun-modifications-test-5-new.json".format(path_to_dir), all_test)
+
+
+main()
