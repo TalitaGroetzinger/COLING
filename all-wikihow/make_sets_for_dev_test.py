f79f7f8fc12622f6520653e579f1069f64fd2964
Talita Anthonio <talitaanthonio@MacBook-Pro-van-Talita.local>
2020-03-31 23:37:40 +0200

    make files for test, train and dev

--- all-wikihow/make_sets_for_dev_test.py
+++ all-wikihow/make_sets_for_dev_test.py
@@ -1,6 +1,7 @@
 # use this script to use wikihowinstance['loc_in_splits']
 
 from progress.bar import Bar
+import json
 
 
 def make_splits(list_of_wikihow_instances):
@@ -15,31 +16,34 @@ def make_splits(list_of_wikihow_instances):
         elif wikihow_instance['Loc_in_splits'] == 'TEST':
             test.append(wikihow_instance)
         else:
-            wikihow_instance['Loc_in_splits'] == 'TRAIN':
             train.append(wikihow_instance)
-
+    bar.finish()
     return dev, test, train
 
 
 def main():
-    path = '../wikihowtools/data/Wikihow_tokenized_v5_cleaned_tokens_only.json'
+    path = '../wikihowtools/data/Wikihow_tokenized_v5_cleaned_splits_tokens_only.json'
     with open(path, 'r') as json_in:
         list_of_wikihow_instances = json.load(json_in)
+
+    print(len(list_of_wikihow_instances))
+
+    print("make the splits ... ")
     dev, test, train = make_splits(list_of_wikihow_instances)
 
-    print("Train instances: {0}, Perc: {1}".len(train),
-          len(train)/len(list_of_wikihow_instances))
-    print("Dev instances: {0}, Perc: {1}".len(dev), len(
-        dev)/len(list_of_wikihow_instances))
-    print("Test instances: {0}, Perc: {1}".len(test), len(
-        test)/len(list_of_wikihow_instances))
+    print("Train instances: {0}, Perc: {1}".format(len(train),
+                                                   len(train)/len(list_of_wikihow_instances)))
+    print("Dev instances: {0}, Perc: {1}".format(len(dev), len(
+        dev)/len(list_of_wikihow_instances)))
+    print("Test instances: {0}, Perc: {1}".format(len(test), len(
+        test)/len(list_of_wikihow_instances)))
 
     with open("wikihow-dev.json", 'w') as json_out:
-        json.dump(json_out, dev)
+        json.dump(dev, json_out)
     with open("wikihow-test.json", 'w') as json_out:
-        json.dump(json_out, test)
+        json.dump(test, json_out)
     with open("wikihow-train.json", 'w') as json_out:
-        json.dump(json_out, train)
+        json.dump(train, json_out)
 
 
 main()
