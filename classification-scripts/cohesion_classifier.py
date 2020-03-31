ef70db579bbb6e467172363abf7fa15f05a072b0
Talita Anthonio <talitaanthonio@MacBook-Pro-van-Talita.local>
2020-03-31 14:01:48 +0200

    finish data functions in cohesion_classifier.py

--- classification-scripts/cohesion_classifier.py
+++ classification-scripts/cohesion_classifier.py
@@ -50,102 +50,66 @@ def get_most_informative_features(classifier, vec, top_features=10):
         vec.get_feature_names(), pos_class_prob_sorted[:top_features]))
 
 
-# let op dat er bij deze functie ook gebruik wordt gemaakt van de matches
-# voor context
-def get_docs_labels(list_of_wikihow_instances, context=False, diff_noun_file=True):
+def join_data(x):
+    return ' '.join(x)
+
+
+def get_xy(list_of_wikihow_instances, use_context='context'):
     X = []
     Y = []
     for wikihow_instance in list_of_wikihow_instances:
-        if context:
-            print("use context ... ")
-            source_context_first = wikihow_instance['Source_Context_5_Processed']
-            source_context = word_tokenize(source_context_first)
-            target_context_first = wikihow_instance['Target_Context_5_Processed']
-            target_context = word_tokenize(target_context_first)
-            #matches = wikihow_instance['PPDB_Matches']
+        if use_context == 'context':
+            print("use context level")
+            source_context = wikihow_instance['Source_Context_5_Processed']
+            target_context = wikihow_instance['Target_Context_5_Processed']
             X.append(source_context)
             Y.append(0)
             X.append(target_context)
             Y.append(1)
-        else:
-            print("do not use context")
-            if diff_noun_file:
-                print("use different noun modifications")
-                source_tokenized = [pair[0]
-                                    for pair in wikihow_instance['Source_tagged']]
-                target_tokenized = [pair[0]
-                                    for pair in wikihow_instance['Target_Tagged']]
-            else:
-                source_tokenized = [pair[0]
-                                    for pair in wikihow_instance['Source_Line_Tagged']]
-                target_tokenized = [pair[0]
-                                    for pair in wikihow_instance['Target_Line_Tagged']]
-            X.append(source_tokenized)
+        elif use_context == 'context-matches':
+            print("use context plus matches")
+            source_context = wikihow_instance['Source_Context_5_Processed']
+            target_context = wikihow_instance['Target_Context_5_Processed']
+            matches = wikihow_instance['PPDB_Matches']
+
+            new_source = mark_cases(source_context, matches, source=True)
+            new_target = mark_cases(target_context, matches, source=False)
+
+            X.append(new_source)
             Y.append(0)
-            X.append(target_tokenized)
+            X.append(new_target)
             Y.append(1)
+
+        else:
+            print("use sentence-level")
+            source_line = ' '.join(
+                [pair[0] for pair in wikihow_instance['Source_Line_Tagged']])
+            target_line = ' '.join(
+                [pair[0] for pair in wikihow_instance['Target_Line_Tagged']])
+            X.append(source_line)
+            Y.append(0)
+            X.append(target_line)
+            Y.append(1)
+    assert len(X) == len(Y)
     return X, Y
 
 
-def preprocess_data(train_diff, dev_diff, test_diff, train_same, dev_same, test_same, use_context=False):
-    with open(train_diff, 'r') as json_in_train:
-        train_open_diff = json.load(json_in_train)
-    with open(dev_diff, 'r') as json_in_dev:
-        dev_open_diff = json.load(json_in_dev)
-    with open(test_diff, 'r') as json_in_test:
-        test_open_diff = json.load(json_in_test)
+def get_data(path_to_train, path_to_dev, path_to_test, context_value='context'):
+    """
+        use_context_level: 'context', 'context-matches', 'sentence'
+    """
+    # load train and test set
+    with open(path_to_train, 'r') as json_in:
+        train = json.load(json_in)
+    with open(path_to_dev, 'r') as json_in:
+        dev = json.load(json_in)
+    with open(path_to_test, 'r') as json_in:
+        test = json.load(json_in)
 
-    with open(train_same, 'r') as json_in_train_same:
-        train_open_same = json.load(json_in_train_same)
-
-    with open(dev_same, 'r') as json_in_dev_same:
-        dev_open_same = json.load(json_in_dev_same)
-
-    with open(test_same, 'r') as json_in_test_same:
-        test_open_same = json.load(json_in_test_same)
-
-    # list_of_wikihow_instances, context=False, use_matches=False, diff_noun_file=True
-    if use_context:
-        Xtrain_diff, Ytrain_diff = get_docs_labels(
-            train_open_diff, context=True)
-        Xdev_diff, Ydev_diff = get_docs_labels(
-            dev_open_diff, context=True)
-        Xtest_diff, Ytest_diff = get_docs_labels(
-            test_open_diff, context=True)
-
-        Xtrain_same, Ytrain_same = get_docs_labels(
-            train_open_same, context=True)
-        Xdev_same, Ydev_same = get_docs_labels(
-            dev_open_same, context=True)
-        Xtest_same, Ytest_same = get_docs_labels(
-            test_open_same, context=True)
-    else:
-        Xtrain_diff, Ytrain_diff = get_docs_labels(
-            train_open_diff, context=False, diff_noun_file=True)
-        Xdev_diff, Ydev_diff = get_docs_labels(
-            dev_open_diff, context=False, diff_noun_file=True)
-        Xtest_diff, Ytest_diff = get_docs_labels(
-            test_open_diff, context=False, diff_noun_file=True)
-
-        Xtrain_same, Ytrain_same = get_docs_labels(
-            train_open_same, context=False, diff_noun_file=False)
-        Xdev_same, Ydev_same = get_docs_labels(
-            dev_open_same, context=False, diff_noun_file=False)
-        Xtest_same, Ytest_same = get_docs_labels(
-            test_open_same, context=False, diff_noun_file=False)
-
-    Xtrain = Xtrain_diff + Xtrain_same
-    Ytrain = Ytrain_diff + Ytrain_same
-    Xdev = Xdev_diff + Xdev_same
-    Ydev = Ydev_diff + Ydev_same
-    Xtest = Xtest_diff + Xtest_same
-    Ytest = Ytest_diff + Ytest_same
-
+    Xtrain, Ytrain = get_xy(train, use_context=context_value)
+    Xdev, Ydev = get_xy(dev, use_context=context_value)
+    Xtest, Ytest = get_xy(test, use_context=context_value)
     return Xtrain, Ytrain, Xdev, Ydev, Xtest, Ytest
-
-
-def join_data(x):
-    return ' '.join(x)
 
 
 def train_classifier(Xtrain, Ytrain, Xdev, Ydev, Xtest, Ytest):
@@ -229,11 +193,10 @@ def main():
     # use_all=false to get path_to_train_diff, path_to_dev_diff, path_to_test_diff, path_to_train_same, path_to_dev_same, path_to_test_same
     train, dev, test = get_paths(use_all=True)
 
-    Xtrain, Ytrain, Xdev, Ydev, Xtest, Ytest = preprocess_data(
-        train_diff, dev_diff, test_diff, train_same, dev_same, test_same, use_context=True)
+    Xtrain, Ytrain, Xdev, Ydev, Xtest, Ytest = get_data(train, dev, test)
 
-    list_of_good_predictions, list_of_bad_predictions = train_classifier(
-        Xtrain, Ytrain, Xdev, Ydev, Xtest, Ytest)
+    # list_of_good_predictions, list_of_bad_predictions = train_classifier(
+    #    Xtrain, Ytrain, Xdev, Ydev, Xtest, Ytest)
 
 
 main()
