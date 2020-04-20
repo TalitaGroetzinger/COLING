1b8dc30256c393b96cb80cc0f3000b13986b6695
Talita Anthonio <talitaanthonio@MacBook-Pro-van-Talita.local>
2020-04-20 13:31:55 +0200

    rename files

--- get-context/remove_timestamps.sh
+++ get-context/remove_timestamps.sh
@@ -1,3 +1,3 @@
-python remove_timestamps.py --input ../classification-scripts/noun-modifications/noun-modifications-test-base-target.json --output ../classification-scripts/noun-modifications/noun-modifications-test-base-target-v2.json 
-python remove_timestamps.py --input ../classification-scripts/noun-modifications/noun-modifications-train-base-target.json --output ../classification-scripts/noun-modifications/noun-modifications-train-base-target-v2.json 
-python remove_timestamps.py --input ../classification-scripts/noun-modifications/noun-modifications-dev-base-target.json --output ../classification-scripts/noun-modifications/noun-modifications-dev-base-target-v2.json 
+python remove_timestamps.py --input ../classification-scripts/noun-modifications/noun-modifications-test-without-match.json --output ../classification-scripts/noun-modifications/noun-modifications-test-without-match-pr.json
+python remove_timestamps.py --input ../classification-scripts/noun-modifications/noun-modifications-dev-without-match.json --output ../classification-scripts/noun-modifications/noun-modifications-dev-without-match-pr.json
+python remove_timestamps.py --input ../classification-scripts/noun-modifications/noun-modifications-train-without-match.json --output ../classification-scripts/noun-modifications/noun-modifications-train-without-match-pr.json
