122ad123f2c7eafc71dce2d2e828cd589a5f7009
Talita Anthonio <talitaanthonio@MacBook-Pro-van-Talita.local>
2020-04-24 09:37:41 +0200

    Stage changes in move-to-server.sh

--- get-context/move-to-server.sh
+++ get-context/move-to-server.sh
@@ -1,1 +1,1 @@
-scp anthonta@wiesenweihe.ims.uni-stuttgart.de:/mount/arbeitsdaten/emmy-noether-roth/mist/Talita/COLING-other
+scp anthonta@wiesenweihe.ims.uni-stuttgart.de:/mount/arbeitsdaten/emmy-noether-roth/mist/Talita/COLING-other/test_context.py . 
