ea964ac108027e684e826ffc07aca9a6fe582fbb
Talita Anthonio <talitaanthonio@MacBook-Pro-van-Talita.local>
2020-04-24 10:14:40 +0200

    add code from notebook to script

--- get-context/get_context_and_length.py
+++ get-context/get_context_and_length.py
@@ -4,33 +4,53 @@ with open('../classification-scripts/subset-train.json', 'r') as json_in:
     content = json.load(json_in)
 
 
-def get_full_article(filename, collection, line_nr='400'):
-    current_line = collection[filename][line_nr]
-    sentence_line_nrs_in_article = [
-        key for key, _ in collection[filename].items()]
-    list_positions = [i for i in range(len(sentence_line_nrs_in_article))]
+def get_left(article_database, filename, line_nr, sentence_nrs):
+    above_line_nrs = list(filter(lambda sentence_nr: int(
+        sentence_nr) <= int(line_nr), sentence_nrs))[::-1]
 
-    line_nr_pos = sentence_line_nrs_in_article.index(str(line_nr))
-    # get everything before timestamp plus the content on line_nr
+    before_lines = []
+    for line_nr in above_line_nrs:
+        line = content[filename][line_nr]
+        # check if there is a timestamp
+        if '## Timestamp' in line:
+            print("Timestamp found ..", line_nr)
+            break
+        before_lines.append(line)
+    return before_lines[::-1]
 
-    left_article_until_timestamp = []
-    for i in range(line_nr_pos-0):
-        key = i+1
-        index_in_dict = sentence_line_nrs_in_article[key]
 
-        if '## Timestamp' not in content[filename][index_in_dict]:
-            line = "{0} {1}".format(
-                index_in_dict, content[filename][index_in_dict])
-            left_article_until_timestamp.append(line)
-        else:
-            print(left_article_until_timestamp)
+def get_right(article_database, filename, line_nr, sentence_nrs):
+    after_line_nrs = filter(lambda sentence_nr: int(
+        sentence_nr) > int(line_nr), sentence_nrs)
+
+    after_lines = []
+
+    for line_nr in after_line_nrs:
+        line = content[filename][line_nr]
+        if '## Timestamp' in line:
+            print("Timestamp found ..", line_nr)
+            break
+        after_lines.append(line)
+    return after_lines
+
+
+def get_full_article(article_database, filename, line_nr='400'):
+    sentence_nrs = [key for key in article_database[filename].keys()]
+    # Take everything above the content that you want including the source or target line.
+    left_content = get_left(article_database, filename, line_nr, sentence_nrs)
+
+    # do the same procedure for the right
+    right_content = get_right(
+        article_database, filename, line_nr, sentence_nrs)
+
+    print(left_content + right_content)
 
 
 def main():
     subset_filenames = ['Be_Resourceful.txt', 'Create_a_Well_Rounded_Approach_for_Getting_Rid_of_Acne.txt', 'Get_a_Big_Warhammer_Bitz_Box.txt', 'Improve_Your_Class_Ranking.txt', 'Clean_a_Sticking_Delta_Soap_Dispenser.txt',
                         'Graciously_Accept_an_Unattractive_Gift_from_the_in_Laws.txt', 'Pay_Off_Student_Loans.txt', "Edit_a_Friend's_Essay.txt", 'Write_Apocalyptic_Stories.txt', 'Make_Your_Room_Look_Nice.txt', 'Deal_with_Sexual_Abuse.txt', 'Have_a_Larger_Vocabulary.txt']
-    for elem in subset_filenames:
-        get_full_article(elem, content)
+    for filename in subset_filenames:
+        get_full_article(content, filename)
         break
 
 
